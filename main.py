e9f99c4b9f8981182511d18d819a1d11523dc7fb
Thuy Nguyen <thuicide@gmail.com>
2022-10-17 16:10:20 -0700

    Exercise 12

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# List to evaluate
 numbers = [    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                [0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
                [1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
@@ -9,9 +10,34 @@ numbers = [    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                [0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
                [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]]
 
+# Number of rows
 rnum = len(numbers)
+
+# Number of columns
 cnum = len(numbers[0])
 
-# ******************************
-# Make your Code
-# ******************************
+# Variable to count cross shapes
+isCross = 0
+
+# Check for cross shapes
+for i in range(rnum):
+    for j in range(cnum):
+        # If 1 is found
+        if numbers[i][j] == 1:
+            # Check for 1 above 1
+            if numbers[i-1][j] == 1:
+                # Check for 1 on left of 1
+                if numbers[i][j-1] == 1:
+                    # Check for 1 on right of 1
+                    if numbers[i][j+1] == 1:
+                        # Check for 1 under 1
+                        if numbers[i+1][j] == 1:
+                            # If all parameters met, count as cross shape
+                            isCross += 1
+
+# Print number of cross shapes in list
+print(isCross)
+
+
+
+
